acaccdb972e2d3d9cf2e81af7304dc482f7db3aa
karenandrea1369 <karenandrea12204@gmail.com>
2019-09-10 14:25:30 -0500

    Interacción acomodada

--- java/main.js
+++ java/main.js
@@ -2,19 +2,26 @@ var buttons = document.querySelectorAll('.manybtn__img');
 var imgButton = document.querySelector('img');
 
 function handleClick (event){
-    //var img = document.querySelector('.firstButton');
     var name = event.target.getAttribute('src')
     var parts = name.split('/');
     var newName = './data/img' + parts[2];
-    var image = document.querySelector(".int__img");
+    var image = document.querySelector(".inter__img");
     image.setAttribute('src', newName);
     console.log(newName);
 }
 
-//function handleClick(event) { //el event es para obtener informacion sobre el evento
 for (var i = 0; i < buttons.length; i++) {
     var button = buttons[i];
     button.addEventListener('click', handleClick );   
 }
-//}
 
+
+var more = document.querySelector('.detail__more');
+var detail = document.querySelector('.detail__descrip');
+
+function handleClick(){
+    detail.classList.toggle('detail__descrip--active');
+    // more.setAttribute('name', 'Mostrar menos -')
+}
+
+more.addEventListener('click', handleClick);
